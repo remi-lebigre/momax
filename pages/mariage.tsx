60addced60b2621d1dafd230990051f7a4611ed9
remi <remi@148.fr>
2021-11-20 17:42:08 +0100

    Add mariage and voyage pages

---
+++ pages/mariage.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Layout from '../components/Layout'
+
+const MariagePage: NextPage = () => {
+  return (
+    <Layout title={'Mariage Morgane & Maxime | Mariage'}>
+      <div className='w-full h-full'>
+        <h1 className='text-center text-4xl'>Mariage</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default MariagePage
---
+++ pages/voyage.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Layout from '../components/Layout'
+
+const VoyagePage: NextPage = () => {
+  return (
+    <Layout title={'Mariage Morgane & Maxime | Voyage'}>
+      <div className='w-full h-full'>
+        <h1 className='text-center text-4xl'>Voyage</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default VoyagePage
