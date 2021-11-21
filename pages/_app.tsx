98f321740ba6f1eba7e9db6846c6e7a4976e2ca1
remi <remi@148.fr>
2021-11-21 17:10:45 +0100

    Add project fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
+import '../styles/fonts.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
