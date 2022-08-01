cd02c2f41a4b35db4eca808989f5452c9a5dd416
remi <remi@148.fr>
2022-08-01 18:34:06 +0200

    feat(jour j): add margin to milestones

--- components/Milestone.tsx
+++ components/Milestone.tsx
@@ -1,14 +1,17 @@
-import { FC } from "react"
+import { FC } from 'react'
 
 export interface MilestoneProps {
-    time: string
-    ampm: 'am' | 'pm'
+  time: string
+  ampm: 'am' | 'pm'
 }
-const Milestone: FC<MilestoneProps> = ({ time, ampm, children }) => <li className="flex items-center gap-4">
-    <span className="basis-1/6">
-        <span className="font-black text-lg">{time}</span>
-        <span className="font-thin">{ampm}</span>
+const Milestone: FC<MilestoneProps> = ({ time, ampm, children }) => (
+  <li className='flex items-center gap-4 mb-1'>
+    <span className='basis-1/6'>
+      <span className='font-black text-lg'>{time}</span>
+      <span className='font-thin'>{ampm}</span>
     </span>
-    <span className="basis-5/6">{children}</span></li>
+    <span className='basis-5/6'>{children}</span>
+  </li>
+)
 
 export default Milestone
