a6d18690fcfcf8ee310d434394edae4340388c7e
remi <remi@148.fr>
2022-08-01 19:07:30 +0200

    feat(animation): images

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,17 +1,29 @@
 import NextImage, { ImageProps } from 'next/image'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 interface ImgProps extends ImageProps {
   noLoadBg?: boolean
 }
-const Image: FC<ImgProps> = ({ src, alt, noLoadBg = false }) => (
-  <NextImage
-    src={src}
-    alt={alt}
-    layout={'fill'}
-    objectFit={'cover'}
-    className={noLoadBg ? '' : 'bg-brown'}
-  />
-)
+const Image: FC<ImgProps> = ({ src, alt, noLoadBg = false }) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  return (
+    <NextImage
+      src={src}
+      alt={alt}
+      layout={'fill'}
+      objectFit={'cover'}
+      className={
+        noLoadBg
+          ? ''
+          : `bg-brown transform transition-opacity opacity-0 duration-700 ${
+              isLoaded && 'opacity-100'
+            }`
+      }
+      onLoadingComplete={() => {
+        setIsLoaded(true)
+      }}
+    />
+  )
+}
 
 export default Image
